777fca4cd5fa6967564b49ec485fd8e027d2f687
Rohith2408 <kumarrohith24081999@gmail.com>
2024-10-04 01:13:12 +0530

    added google tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Tokenomics from './components/Tokenomics';
 import Section1 from './components/Section1';
 import Memes from './components/Memes';
 import About from './components/About';
